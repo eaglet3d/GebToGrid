d68f8b49300bf177cd31bea374b9a9672da1bd8b
Ralph A. Navarro Jr <ralph@navarrocomputing.com>
2016-11-04 02:18:04 -0400

    Add indents to spock code

--- CodeExamples/geb-example-gradle/src/test/groovy/GebishOrgSpec.groovy
+++ CodeExamples/geb-example-gradle/src/test/groovy/GebishOrgSpec.groovy
@@ -4,20 +4,20 @@ class GebishOrgSpec extends GebReportingSpec {
 
     def "can get to the current Book of Geb"() {
         when:
-        to GebishOrgHomePage
+            to GebishOrgHomePage
 
         and: //hover over to expand the menu
-        interact {
-            moveToElement(manualsMenu.toggle)
-        }
+            interact {
+                moveToElement(manualsMenu.toggle)
+            }
 
         then: //first link is for the current manual
-        manualsMenu.links[0].text().endsWith("- CURRENT")
+            manualsMenu.links[0].text().endsWith("- CURRENT")
 
         when:
-        manualsMenu.links[0].click()
+            manualsMenu.links[0].click()
 
         then:
-        at TheBookOfGebPage
+            at TheBookOfGebPage
     }
 }
